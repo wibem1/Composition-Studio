b1ede08e3f4bba90ace1c1b32ff042ba5149d194
wibem1 <wibem1@googlemail.com>
2026-09-15 11:07:44 +0200

    V0.7.1 activate functional DAW bindings

--- native/Sources/EngineHooks.swift
+++ native/Sources/EngineHooks.swift
@@ -17,35 +17,25 @@ final class EngineHooks: NSObject {
     }
 
     @objc private func applicationDidLaunch() {
-        DispatchQueue.main.async { [weak self] in
-            self?.connectUI()
-        }
+        DispatchQueue.main.async { [weak self] in self?.connectUI() }
     }
 
     private func connectUI() {
         let engine = CompositionStudioEngine.shared
         let ok = engine.initialize()
-
         guard let vc = NSApp.windows.first?.contentViewController as? StudioViewController else { return }
         controller = vc
         vc.installEngineBindings(engineReady: ok)
-
+        vc.installV071FunctionalBindings()
         timer?.invalidate()
-        timer = Timer.scheduledTimer(
-            timeInterval: 0.05,
-            target: self,
-            selector: #selector(refreshTransport),
-            userInfo: nil,
-            repeats: true
-        )
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(refreshTransport), userInfo: nil, repeats: true)
         refreshTransport()
     }
 
     @objc private func refreshTransport() {
         controller?.refreshEngineUI()
+        controller?.refreshV071FunctionalUI()
     }
 }
 
-func installCompositionStudioEngineHooks() {
-    EngineHooks.shared.install()
-}
+func installCompositionStudioEngineHooks() { EngineHooks.shared.install() }
