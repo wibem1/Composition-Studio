4644e52a498bc23a17a1a439ff9768a8a9a1b4ef
wibem1 <wibem1@googlemail.com>
2026-09-15 13:53:43 +0200

    V1.0 RC: add zero-frame initializers for studio views

---
+++ native/Sources/V100Compatibility.swift
@@ -0,0 +1,13 @@
+import AppKit
+
+private extension RCArrangementView {
+    convenience init() { self.init(frame: .zero) }
+}
+
+private extension RCPianoRollView {
+    convenience init() { self.init(frame: .zero) }
+}
+
+private extension RCNotationView {
+    convenience init() { self.init(frame: .zero) }
+}
